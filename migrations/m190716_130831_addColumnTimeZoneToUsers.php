2782b2db02fbec5c74e177aab3c31c9f8600f841
KirylJazzSax <matsak_92@mail.ru>
2019-07-16 16:14:12 +0300

    Миграции: Создание таблиц activity и time_zone

---
+++ migrations/m190716_130831_addColumnTimeZoneToUsers.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190716_130831_addColumnTimeZoneToUsers
+ */
+class m190716_130831_addColumnTimeZoneToUsers extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('Users', 'time_zone_id', $this->integer(11)->notNull());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('Users', 'time_zone_id');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190716_130831_addColumnTimeZoneToUsers cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
